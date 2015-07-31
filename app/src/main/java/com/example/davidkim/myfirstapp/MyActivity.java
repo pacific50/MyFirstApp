6c48fe2c151fe8241674362d72e8be8fbf9eb02c
pacific50 <pacific50@yahoo.com>
2015-07-31 14:29:16 -0700

    Added sendMessage class.

--- app/src/main/java/com/example/davidkim/myfirstapp/MyActivity.java
+++ app/src/main/java/com/example/davidkim/myfirstapp/MyActivity.java
@@ -1,11 +1,19 @@
 package com.example.davidkim.myfirstapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
+
+    public class MyActivity extends ActionBarActivity {
+        public final static String EXTRA_MESSAGE = "com.example.davidkim.myfirstapp";
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +41,13 @@ public class MyActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
