1fa680fd2e467d4b6f2b1cb8d45c95961122a478
pacific50 <pacific50@yahoo.com>
2015-07-31 15:31:35 -0700

    Added DisplayMessageActivity.

--- app/src/main/java/com/example/davidkim/myfirstapp/MyActivity.java
+++ app/src/main/java/com/example/davidkim/myfirstapp/MyActivity.java
@@ -2,7 +2,6 @@ package com.example.davidkim.myfirstapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,10 +9,7 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MyActivity extends AppCompatActivity {
-
-    public class MyActivity extends ActionBarActivity {
-        public final static String EXTRA_MESSAGE = "com.example.davidkim.myfirstapp";
-    }
+    public final static String EXTRA_MESSAGE = "com.example.davidkim.myfirstapp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
